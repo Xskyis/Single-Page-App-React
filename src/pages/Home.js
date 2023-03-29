83732b4dc7077af2483edfb394eddd190a95a10a
Xskyis <nabilafgarezaafgarza@gmail.com>
2023-03-29 07:20:17 +0700

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Home extends React.Component{ 
+    render(){   
+        return( 
+            <div className="container p-1">   
+                <div className="alert alert-success">   
+                    Ini adalah halaman home
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
